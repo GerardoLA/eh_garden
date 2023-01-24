1be8f06703f640fc52589821f9da32c903d75f43
Usuario <Usuario@DESKTOP-AB3UK2C>
2023-01-24 20:34:53 +0100

    Del todo

--- src/principal/GestorArboles.java
+++ src/principal/GestorArboles.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 public class GestorArboles {
@@ -50,25 +49,21 @@ public class GestorArboles {
 					
 					insertarArbol();
 					System.out.println("Yes!! Árbol introducido correctamente"); 
-					
 					break; 
 					
 				case ELIMINAR_ARBOL:
 					
 					eliminarArbol();
 					System.out.println(  "eliminado");
-					
 					break;
 					
 				case MODIFICAR:
 					
 					modificar();
 					System.out.println("modificado ok!");
-					
 					break;
 					
 				case VISUALIZAR:
-					
 					visualizar();
 					break;
 					
@@ -90,66 +85,6 @@ public class GestorArboles {
 			e.printStackTrace();
 
 		}
-	}
-
-	private static void visualizar() throws SQLException {
-		
-		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
-		PreparedStatement pst = con.prepareStatement("INSERT INTO arboles VALUES (null,?,?,?,?,?)");
-		System.out.println("cuarta opcion seleccionada\n");
-		String sentenciaSelect = "SELECT *FROM arboles";
-		ResultSet resultado = pst.executeQuery(sentenciaSelect);
-		while(resultado.next()) {
-			System.out.println(resultado.getInt(1)+" - "+resultado.getString(2));
-		}
-		
-	}
-
-	private static void modificar() throws SQLException {
-		Arbol arbol = new Arbol();
-		Scanner scan =new Scanner(System.in);
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
-		PreparedStatement pstModif = con.prepareStatement("UPDATE arboles set nombre_comun=?,nombre_cientifico=?,habitat=?,altura=?,origen=? WHERE id=?");
-		
-		System.out.println("Modicifando arbol...");
-		
-		System.out.println("introduce id del arbol a modificar :");
-		arbol.setId(Integer.parseInt(scan.nextLine()));
-		System.out.println("introduce nombre nuevo");
-		arbol.setNombreComun(scan.nextLine());
-		System.out.println("Nombre cientifico nuevo :");
-		arbol.setNombrecientifico(scan.nextLine());
-		System.out.println("Habitat nuevo : ");
-		arbol.setHabitat(scan.nextLine());
-		System.out.println("Altura nueva : ");
-		arbol.setAltura(Integer.parseInt(scan.nextLine()));
-		System.out.println("Origen : ");
-		arbol.setOrigen(scan.nextLine());
-		
-		pstModif.setString(1,arbol.getNombreComun());
-		pstModif.setString(2,arbol.getNombrecientifico());
-		pstModif.setString(3, arbol.getHabitat());
-		pstModif.setInt(4, arbol.getAltura());
-		pstModif.setString(5, arbol.getOrigen());
-		pstModif.setInt(6, arbol.getId());
-		
-		pstModif.executeUpdate();
-		
-	}
-
-	private static void eliminarArbol() throws SQLException {
-		Arbol arbol=new Arbol();
-		Scanner scan =new Scanner(System.in);
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
-		PreparedStatement pstElim = con.prepareStatement("DELETE FROM arboles WHERE id=?");
-		System.out.println("Eliminando un arbol...");
-		System.out.println("Introduce la id del arbol a eliminar");
-		
-		arbol.setId(Integer.parseInt(scan.nextLine()));
-		pstElim.setInt(1, arbol.getId());
-		pstElim.execute();
-		
 	}
 
 	private static void insertarArbol() throws SQLException {
@@ -181,4 +116,74 @@ public class GestorArboles {
 		pst.execute();
 		
 	}
+	
+	
+	private static void modificar() throws SQLException {
+		
+		Arbol arbol = new Arbol();
+		Scanner scan =new Scanner(System.in);
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
+		PreparedStatement pstModif = con.prepareStatement("UPDATE arboles set nombre_comun=?,nombre_cientifico=?,habitat=?,altura=?,origen=? WHERE id=?");
+		
+		System.out.println("Modicifando arbol...");
+		
+		System.out.println("introduce id del arbol a modificar :");
+		arbol.setId(Integer.parseInt(scan.nextLine()));
+		System.out.println("introduce nombre nuevo");
+		arbol.setNombreComun(scan.nextLine());
+		System.out.println("Nombre cientifico nuevo :");
+		arbol.setNombrecientifico(scan.nextLine());
+		System.out.println("Habitat nuevo : ");
+		arbol.setHabitat(scan.nextLine());
+		System.out.println("Altura nueva : ");
+		arbol.setAltura(Integer.parseInt(scan.nextLine()));
+		System.out.println("Origen : ");
+		arbol.setOrigen(scan.nextLine());
+		
+		pstModif.setString(1,arbol.getNombreComun());
+		pstModif.setString(2,arbol.getNombrecientifico());
+		pstModif.setString(3, arbol.getHabitat());
+		pstModif.setInt(4, arbol.getAltura());
+		pstModif.setString(5, arbol.getOrigen());
+		pstModif.setInt(6, arbol.getId());
+		
+		pstModif.executeUpdate();
+		
+	}
+	
+	private static void eliminarArbol() throws SQLException {
+		
+		Arbol arbol=new Arbol();
+		Scanner scan =new Scanner(System.in);
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
+		PreparedStatement pstElim = con.prepareStatement("DELETE FROM arboles WHERE id=?");
+		
+		System.out.println("Eliminando un arbol...");
+		System.out.println("Introduce la id del arbol a eliminar");
+		
+		arbol.setId(Integer.parseInt(scan.nextLine()));
+		pstElim.setInt(1, arbol.getId());
+		pstElim.execute();
+		
+	}
+	
+	private static void visualizar() throws SQLException {
+		
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/eh_garden", "root", "");
+		PreparedStatement pst = con.prepareStatement("INSERT INTO arboles VALUES (null,?,?,?,?,?)");
+		System.out.println("cuarta opcion seleccionada\n");
+		String sentenciaSelect = "SELECT *FROM arboles";
+		ResultSet resultado = pst.executeQuery(sentenciaSelect);
+		while(resultado.next()) {
+			System.out.println(resultado.getInt(1)+" - "+resultado.getString(2));
+		}
+		
+	}
+
+
+
+
+
+
 }
